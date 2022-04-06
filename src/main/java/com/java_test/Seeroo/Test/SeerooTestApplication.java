80ebe58d1b3894c56bbf8dd1304db28a76f25c8b
Pranav <pranav.p@seeroo.com>
2022-04-07 00:36:13 +0530

    added Seeroo Test Initial

---
+++ src/main/java/com/java_test/Seeroo/Test/SeerooTestApplication.java
@@ -0,0 +1,13 @@
+package com.java_test.Seeroo.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerooTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerooTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_test/Seeroo/Test/SeerooTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_test.Seeroo.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerooTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
